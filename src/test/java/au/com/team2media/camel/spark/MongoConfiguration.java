2559fbc5f67a095a4b6ac001d46bb39b9c5f2237
joelatty <joelatty@gmail.com>
2015-03-20 15:52:38 +1100

    WIP

--- src/test/java/au/com/team2media/camel/spark/MongoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package au.com.team2media.camel.spark;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("camel-config.xml")
-public class MongoConfiguration {
-}
